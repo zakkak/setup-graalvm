2b8ee34a304ab8665efaf1afcafdd492ad361fe7
Foivos Zakkak <fzakkak@redhat.com>
2023-10-17 11:41:11 +0300

    Support version prefix for Mandrel
    This enables setting version to `23` or `23.0` and setup-graalvm will
    find the latest mandrel version on that train,
    e.g. `mandrel-23.1.0.0-Final` for `23` and `mandrel-23.0.1.2-Final` for
    `23.0`.
    
    This feature is useful for CI jobs testing an application/framework
    working with an older Mandrel LTS version while still using the latest
    CPU update of it.

--- src/mandrel.ts
+++ src/mandrel.ts
@@ -1,6 +1,7 @@
 import * as c from './constants'
-import {downloadExtractAndCacheJDK, getLatestRelease} from './utils'
+import {downloadExtractAndCacheJDK, getLatestRelease, getMatchingTags} from './utils'
 import {downloadTool} from '@actions/tool-cache'
+import {gt as semverGt, valid as semverValid} from 'semver'
 
 const MANDREL_REPO = 'mandrel'
 const MANDREL_TAG_PREFIX = c.MANDREL_NAMESPACE
@@ -45,6 +46,11 @@ async function setUpMandrelRelease(
   version: string,
   javaVersion: string
 ): Promise<string> {
+  const dotsCount = version.split('.').length - 1
+  if (dotsCount < 3) {
+    version = await findLatestMandrelVersion(version)
+  }
+
   const identifier = determineMandrelIdentifier(version, javaVersion)
   const downloadUrl = `${MANDREL_DL_BASE}/${MANDREL_TAG_PREFIX}${version}/${identifier}${c.GRAALVM_FILE_EXTENSION}`
   const toolName = determineToolName(javaVersion)
@@ -54,6 +60,34 @@ async function setUpMandrelRelease(
     version
   )
 }
+
+async function findLatestMandrelVersion(
+  versionPrefix: string
+): Promise<string> {
+  const matchingRefs = await getMatchingTags(
+    `${MANDREL_TAG_PREFIX}-${versionPrefix}`
+  )
+  const lowestNonExistingVersion = '0.0.1'
+  let highestVersion = lowestNonExistingVersion
+  const versionNumberStartIndex = `refs/tags/${MANDREL_TAG_PREFIX}`.length
+  for (const matchingRef of matchingRefs) {
+    // Mandrel tags are of the form 'mandrel-21.3.0.0-Final'
+    const currentVersion = matchingRef.split('-')[1]
+    if (
+      semverValid(currentVersion) &&
+      semverGt(currentVersion, highestVersion)
+    ) {
+      highestVersion = currentVersion
+    }
+  }
+  if (highestVersion === lowestNonExistingVersion) {
+    throw new Error(
+      `Unable to find the latest Mandrel version starting with '${versionPrefix}'. Please make sure version is set correctly. ${c.ERROR_HINT}`
+    )
+  }
+  return highestVersion
+}
+
 
 function determineMandrelIdentifier(
   version: string,
